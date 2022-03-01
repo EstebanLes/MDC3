41a925ac74633d34904532d1af0c289aad6d1c62
EstebanLes <estebanles22@gamail.com>
2022-03-01 10:32:56 -0300

    Material Desing y Conexion Sql

---
+++ app/src/main/java/com/example/mdc3/ScrollingActivity.kt
@@ -0,0 +1,55 @@
+package com.example.mdc3
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import com.example.mdc3.databinding.ActivityScrollingBinding
+import com.google.android.material.bottomappbar.BottomAppBar
+
+class ScrollingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScrollingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityScrollingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fab.setOnClickListener {
+            if (binding.bottomAppBar.fabAlignmentMode == (BottomAppBar.FAB_ALIGNMENT_MODE_CENTER)){
+                binding.bottomAppBar.fabAlignmentMode = (BottomAppBar.FAB_ALIGNMENT_MODE_END)
+            }else{
+                binding.bottomAppBar.fabAlignmentMode = (BottomAppBar.FAB_ALIGNMENT_MODE_CENTER)
+            }
+        }
+        //snackbar para mostrar mensaje como un toas el set anchor hace que se posicione por arriba del fab
+        binding.bottomAppBar.setOnClickListener {
+            Snackbar.make(binding.root, R.string.message_action_success, Snackbar.LENGTH_LONG)
+                .setAnchorView(binding.fab)
+                .show()
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_scrolling, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
